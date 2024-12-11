858e4216338fd64c48423062f0e0281643f551a7
Eric Lifs <eric.lifszyc@valtech.com>
2024-12-11 12:12:20 -0300

    feat: add icons components

---
+++ src/icons/PlayIcon.tsx
@@ -0,0 +1,18 @@
+export default function PlayIcon({ size }: { size: number }) {
+  return (
+    <svg
+      width={size}
+      height={size}
+      viewBox="0 0 14 16"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        fill-rule="evenodd"
+        clip-rule="evenodd"
+        d="M11.9423 8.2363L2.625 1.875V14.125L11.9423 8.2363Z"
+      />
+    </svg>
+  );
+}
+
---
+++ src/icons/PlusIcon.tsx
@@ -0,0 +1,15 @@
+export default function PlusIcon({ size }: { size: number }) {
+  return (
+    <svg
+      width={size}
+      height={size}
+      viewBox="0 0 15 14"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path d="M7.5 0V14" />
+      <path d="M14.5 7L0.5 7" />
+    </svg>
+  );
+}
+
